16ce9aa9d41d1d841bab67ea724d1bae5d5229f4
Matt Layman <matthewlayman@gmail.com>
2016-03-05 15:51:14 -0500

    Fake the collection of samples.

--- lights/management/commands/sample.py
+++ lights/management/commands/sample.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
+import random
 import time
 
 from django.core.management.base import BaseCommand
+
+from lights.models import LightSample
 
 
 class Command(BaseCommand):
@@ -11,6 +14,10 @@ class Command(BaseCommand):
             while True:
                 print('Collecting a sample...')
                 # TODO: Connect to an Arduino via PySerial and measure a sample.
+                # Fake a data sample.
+                state = random.choice([True, False])
+                print('Fake Arduino is {}'.format('on' if state else 'off'))
+                LightSample.objects.create(state=state)
                 time.sleep(1)
         except KeyboardInterrupt:
             print('\nBye.')
